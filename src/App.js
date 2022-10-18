0ca85fc3c6f348981121b7f7bfd926d0c7be0a84
20CS083JiyaShah <91939547+20CS083JiyaShah@users.noreply.github.com>
2022-10-18 18:06:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,99 @@ import Login from "./pages/Login";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import LoginTeacher from "./pages/LoginTeacher";
+import FirebaseConfig from "./FirebaseConfig";
+import React,{useState,useEffect} from "react";
 
-function App() {
+import { initializeApp } from 'firebase/app'; 
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const  App = ()  => {
+
+  const [user,setUser] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
+
+  const clearInputs=()=>{
+    setEmail('');
+    setPassword('');
+  }
+  
+  const clearErrors=()=>{
+    setEmailError('');
+    setPassword('');
+  }
+
+  const handleLogin = () =>{
+    FirebaseConfig
+      .auth()
+      .signInWithEmailAndPassword(email,password)
+      .catch((err) => {
+        switch (err.code){
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleSignUp=()=>{
+    FirebaseConfig
+      .auth()
+      .createUserWithEmailAndPassword(email,password)
+      .catch((err) => {
+        switch (err.code){
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;  
+        }
+      });
+  };
+
+  const handleLogout=()=>{
+    FirebaseConfig.auth().signOut();
+  };
+
+  const authListner=()=>{
+    FirebaseConfig.auth().onAuthStateChanged((user)=> {
+      if(user){
+        setUser(user);
+      }
+      else {
+        setUser(" ");
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListner();
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <Login />
+            <Login 
+              email ={email}
+              setEmail = {setEmail}
+              password = {password}
+              setPassword = {setPassword}
+              handleLogin = {handleLogin}
+              handleSignUp = {handleSignUp}
+              hasAccount = {hasAccount}
+              setHasAccount = {setHasAccount}
+              emailError = {emailError}
+              passwordError = {passwordError}
+            />
           </Route>
           <Route exact path="/signup">
             <SignUp />
